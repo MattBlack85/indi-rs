33a30b1a1731921ed41fbbee0886ed7fa99a3ece
Mattia Procopio <promat85@gmail.com>
2023-01-27 20:44:05 +0100

    Reduce the CPU utilisation by 100%

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
+use std::collections::VecDeque;
 use std::io::prelude::*;
 use std::net::TcpStream;
 use std::sync::{Arc, RwLock};
-
-use std::collections::VecDeque;
+use std::{thread, time};
 
 fn main() {
     // Open a TCP connection with INDI server
@@ -17,31 +17,34 @@ fn main() {
 
     // Say hello to INDI server
     stream.write(b"<getProperties version='1.7'/>").unwrap();
-    
+
     std::thread::spawn(move || {
-	loop {
-	    let mut rqueue = q.write().unwrap();
-	    match rqueue.pop_front() {
-		Some((m, s)) => {
-		    match roxmltree::Document::parse(std::str::from_utf8(&m[0..s]).unwrap()) {
-			Ok(doc) => println!("XML Doc: {:?}\n\n\n", &doc),
-			Err(_) => {
-			    println!("Bad XML");
-			    // TODO: if we cannot convert to XML we likely have a partial message,
-			    // the next one needs to be parsed and joined with the previous one to
-			    // check if it's now valid. Continue until the XML doc is valid.
-			},
-		    };
-		},
-		None => (),
-	    }
-	};
+        loop {
+            {
+                let mut rqueue = q.write().unwrap();
+                match rqueue.pop_front() {
+                    Some((m, s)) => {
+                        match roxmltree::Document::parse(std::str::from_utf8(&m[0..s]).unwrap()) {
+                            Ok(doc) => println!("XML Doc: {:?}\n\n\n", &doc),
+                            Err(_) => {
+                                println!("Bad XML");
+                                // TODO: if we cannot convert to XML we likely have a partial message,
+                                // the next one needs to be parsed and joined with the previous one to
+                                // check if it's now valid. Continue until the XML doc is valid.
+                            }
+                        };
+                    }
+                    None => (),
+                }
+            }
+            thread::sleep(time::Duration::from_millis(10));
+        }
     });
-    
+
     loop {
-	let mut buf = [0; 2048];
-	let read_bytes = stream.read(&mut buf).unwrap();
-	let mut wqueue = queue.write().unwrap();
-	wqueue.push_back((buf, read_bytes));
-    };
+        let mut buf = [0; 2048];
+        let read_bytes = stream.read(&mut buf).unwrap();
+        let mut wqueue = queue.write().unwrap();
+        wqueue.push_back((buf, read_bytes));
+    }
 }
