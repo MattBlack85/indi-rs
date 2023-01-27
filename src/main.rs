04893034856ce5beab99dba56f94eadf4d1a7841
Mattia Procopio <promat85@gmail.com>
2023-01-27 20:29:44 +0100

    Switch back to sync, use VecDeque and threads to read indi messages

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,47 @@
-use tokio::net::TcpStream;
-use tokio::io::AsyncWriteExt;
-use std::error::Error;
-use std::{io, str};
-use tokio::task;
+use std::io::prelude::*;
+use std::net::TcpStream;
+use std::sync::{Arc, RwLock};
 
+use std::collections::VecDeque;
 
+fn main() {
+    // Open a TCP connection with INDI server
+    let mut stream = TcpStream::connect("127.0.0.1:7624").unwrap();
 
-fn process_xml(bytes: usize, content: &[u8]) {
-    println!("read {} bytes", bytes);
-    let s = str::from_utf8(content).unwrap();
-    let s = format!("<fake>{}</fake>", s);
-    println!("result: {}", s);
-    let doc = roxmltree::Document::parse(&s).unwrap();
-    println!("doc {:?}", doc);
-}
+    // Create the "queue" to store INDI messages, since the queue will be shared between threads
+    // it needs to be ARCed.
+    let queue: Arc<RwLock<VecDeque<([u8; 2048], usize)>>> = Arc::new(RwLock::new(VecDeque::new()));
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
-    // Connect to a peer
-    let mut stream = TcpStream::connect("127.0.0.1:7624").await?;
- 
-    // Say welcome to indiserver, otherwise we are not getting any message
-    stream.write_all(b"<getProperties version='1.7'/>").await?;
+    // Clone the queue for the reader thread
+    let q = Arc::clone(&queue);
+
+    // Say hello to INDI server
+    stream.write(b"<getProperties version='1.7'/>").unwrap();
+    
+    std::thread::spawn(move || {
+	loop {
+	    let mut rqueue = q.write().unwrap();
+	    match rqueue.pop_front() {
+		Some((m, s)) => {
+		    match roxmltree::Document::parse(std::str::from_utf8(&m[0..s]).unwrap()) {
+			Ok(doc) => println!("XML Doc: {:?}\n\n\n", &doc),
+			Err(_) => {
+			    println!("Bad XML");
+			    // TODO: if we cannot convert to XML we likely have a partial message,
+			    // the next one needs to be parsed and joined with the previous one to
+			    // check if it's now valid. Continue until the XML doc is valid.
+			},
+		    };
+		},
+		None => (),
+	    }
+	};
+    });
     
     loop {
-        // Wait for the socket to be readable
-        stream.readable().await?;
-
-        // Creating the buffer **after** the `await` prevents it from
-        // being stored in the async task.
-	let mut buf = Vec::with_capacity(16384);
-
-        // Try to read data, this may still fail with `WouldBlock`
-        // if the readiness event is a false positive.
-        match stream.try_read_buf(&mut buf) {
-            Ok(0) => break,
-            Ok(n) => task::spawn_blocking(move || {
-		process_xml(n, &buf);
-            }).await?,
-            Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
-		continue;
-            }
-            Err(e) => {
-                return Err(e.into());
-            }
-        }
-    }
-
-    Ok(())
+	let mut buf = [0; 2048];
+	let read_bytes = stream.read(&mut buf).unwrap();
+	let mut wqueue = queue.write().unwrap();
+	wqueue.push_back((buf, read_bytes));
+    };
 }
